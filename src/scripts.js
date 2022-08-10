bf4ffe9133d53648d618674355bbeefffd33b7dc
rchung29 <107057931+rchung29@users.noreply.github.com>
2022-08-10 15:30:00 -0400

    commit?

--- src/scripts.js
+++ src/scripts.js
@@ -4,3 +4,50 @@ const menu = document.querySelector('#menu'); // Menu
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('animatecss-fadeInUp');
+    }
+  });
+});
+
+const observer2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('animatecss-fadeInLeft');
+    }
+  });
+});
+
+const observer3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('animatecss-fadeInRight');
+    }
+  });
+});
+
+
+observer.observe(document.querySelector('#fadeInUp'));
+observer.observe(document.querySelector('#fadeInUp2'));
+observer2.observe(document.querySelector('#fadeInLeft'));
+observer2.observe(document.querySelector('#fadeInLeft2'));
+observer2.observe(document.querySelector('#fadeInLeft3'));
+observer2.observe(document.querySelector('#fadeInLeft4'));
+observer2.observe(document.querySelector('#fadeInLeft5'));
+observer2.observe(document.querySelector('#fadeInLeft6'));
+observer2.observe(document.querySelector('#fadeInLeft7'));
+observer3.observe(document.querySelector('#fadeInRight'));
+observer3.observe(document.querySelector('#fadeInRight2'));
+observer3.observe(document.querySelector('#fadeInRight3'));
